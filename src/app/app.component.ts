f9d6664bb8ced91cdf141812ba1e1186a9c3113a
Ivan Piven <piviv64@gmail.com>
2018-12-02 20:06:29 +0200

    Adding firebase in project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Platform, NavController, MenuController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import firebase from 'firebase';
 
 import { TabsPage } from '../pages/tabs/tabs';
 import { SigninPage } from '../pages/signin/signin';
@@ -17,6 +18,10 @@ export class MyApp {
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, 
     private menuCtrl: MenuController) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyCW62fiMiIv-pLWajB0rwNrOO2YProe6Nc",
+        authDomain: "ionic2-recipesbook-15a85.firebaseapp.com"
+      });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
