9c5558d8a5dd808207f425a9a50fb844c98f501c
Ivan Piven <piviv64@gmail.com>
2018-12-04 23:55:41 +0200

    Refining the Signii Page, dialog with user in autentification

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -1,21 +1,34 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms'
 import { AuthService } from '../../services/auth';
+import { AlertController, LoadingController } from 'ionic-angular';
 
 @Component({
   selector: 'page-signin',
   templateUrl: 'signin.html',
 })
 export class SigninPage {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService,
+              private alertCtrl: AlertController,
+              private loadinngCtrl: LoadingController) {}
   
   onSignin(form: NgForm) {
+    const loading = this.loadinngCtrl.create({
+      content: 'Signing you in...'
+    });
+    loading.present();
     this.authService.signin(form.value.email, form.value.password)
     .then(data =>{
-      console.log(data)
+      loading.dismiss();
     })
     .catch(error => {
-      console.log(error);
-    })
+      loading.dismiss();
+      const alert = this.alertCtrl.create({
+      title: 'Signin filed',
+      message: error.message,
+      buttons: ['Ok!']
+      });
+      alert.present();
+    });
   }
 }
