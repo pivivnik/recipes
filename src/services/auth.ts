744f0fcfae24320d6d00f523e428b7bfeedb846a
Ivan Piven <piviv64@gmail.com>
2018-12-04 21:34:55 +0200

    Implementing the Signin process

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,11 @@
 import firebase from 'firebase'
+import { SigninPage } from '../pages/signin/signin';
 
 export class AuthService {
     signup(email: string, password: string) {
        return firebase.auth().createUserWithEmailAndPassword(email, password);
-}
+    }
+    signin(email: string, password: string) {
+       return firebase.auth().signInWithEmailAndPassword(email, password);
+    }
 }
